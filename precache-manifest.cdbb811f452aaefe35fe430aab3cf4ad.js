e3eaeaa020f25832767ef1803fe4f0a6843f0a1a
vaibhavmittal1998 <vaibhav.mittal1998@gmail.com>
2021-01-06 02:03:22 +0530

    Updates

---
+++ precache-manifest.cdbb811f452aaefe35fe430aab3cf4ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c87b82ba2699ff53ced8c925718fddec",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "2b5bad17a5f8bf430643",
+    "url": "/Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/Portfolio/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Portfolio/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b5bad17a5f8bf430643",
+    "url": "/Portfolio/static/js/main.34ac4362.chunk.js"
+  },
+  {
+    "revision": "b4f6ea8748b4b447c1e0",
+    "url": "/Portfolio/static/js/runtime-main.d4d59c43.js"
+  }
+]);
